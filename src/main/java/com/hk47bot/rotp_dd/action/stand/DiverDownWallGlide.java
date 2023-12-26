3691fa98b1e4ebf07cad3208da2f872783e234e3
Yarost228 <121579753+Yarost228@users.noreply.github.com>
2023-12-26 22:16:15 +0300

    Now it moves faster and in any direction

--- src/main/java/com/hk47bot/rotp_dd/action/stand/DiverDownWallGlide.java
+++ src/main/java/com/hk47bot/rotp_dd/action/stand/DiverDownWallGlide.java
@@ -3,15 +3,24 @@ package com.hk47bot.rotp_dd.action.stand;
 import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.action.ActionTarget;
 import com.github.standobyte.jojo.action.stand.StandEntityAction;
+import com.github.standobyte.jojo.entity.stand.StandEntity;
+import com.github.standobyte.jojo.entity.stand.StandEntityTask;
+import com.github.standobyte.jojo.entity.stand.StandRelativeOffset;
 import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 
+import net.minecraft.block.Block;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.world.World;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 
 public class DiverDownWallGlide extends StandEntityAction {
-    
+    private StandEntity standEntity;
 
     public DiverDownWallGlide(StandEntityAction.Builder builder) {
         super(builder.holdType());
@@ -23,11 +32,14 @@ public class DiverDownWallGlide extends StandEntityAction {
         }
         return ActionConditionResult.NEGATIVE;
     }
+
     @Override
     protected void holdTick(World world, LivingEntity user, IStandPower userPower, int ticksHeld, ActionTarget target, boolean requirementsFulfilled) {
         if (world.isClientSide() && user.horizontalCollision) {
-                Vector3d movement = user.getLookAngle();
-                user.setDeltaMovement(movement.x/3.5, 0, movement.z/3.5);
+            Vector3d movement = user.getLookAngle();
+            user.setDeltaMovement(movement.x / 3, movement.y/3, movement.z / 3);
+
+        }
+
         }
     }
-}
