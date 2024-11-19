b5fe83a2843985ef2df0a7fb504786bf8d43b659
Yarost228 <yarik.alferov1@gmail.com>
2024-11-19 21:52:17 +0300

    Added Spring Legs

---
+++ src/main/java/com/hk47bot/rotp_dd/effects/SpringLegsEffect.java
@@ -0,0 +1,16 @@
+package com.hk47bot.rotp_dd.effects;
+
+import com.github.standobyte.jojo.potion.UncurableEffect;
+import com.github.standobyte.jojo.util.mc.damage.DamageUtil;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.EffectType;
+import net.minecraft.util.DamageSource;
+
+public class SpringLegsEffect extends UncurableEffect {
+    public SpringLegsEffect(EffectType type, int color){super(type, color);}
+    public void applyEffectTick(LivingEntity entity, int amplifier) {
+        if (!entity.level.isClientSide() && entity.isOnGround()){
+            entity.hurt(DamageSource.FALL, 2.0F);
+        }
+    }
+}
