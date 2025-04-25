26da709b76ea022c22e433af4413cc0b7731954c
Yarost228 <yarik.alferov1@gmail.com>
2025-04-25 22:18:20 +0300

    Fixed crash

--- src/main/java/com/hk47bot/rotp_dd/action/stand/DiverDownSpringLegs.java
+++ src/main/java/com/hk47bot/rotp_dd/action/stand/DiverDownSpringLegs.java
@@ -2,7 +2,6 @@ package com.hk47bot.rotp_dd.action.stand;
 
 import com.github.standobyte.jojo.action.ActionConditionResult;
 import com.github.standobyte.jojo.action.ActionTarget;
-import com.github.standobyte.jojo.action.stand.StandEntityAction;
 import com.github.standobyte.jojo.action.stand.StandEntityHeavyAttack;
 import com.github.standobyte.jojo.action.stand.punch.StandEntityPunch;
 import com.github.standobyte.jojo.entity.stand.StandEntity;
@@ -32,14 +31,14 @@ public class DiverDownSpringLegs extends StandEntityHeavyAttack {
 
     @Override
     public StandEntityPunch punchEntity(StandEntity stand, Entity target, StandEntityDamageSource dmgSource) {
+        DiverDownEntity diverDown = (DiverDownEntity) stand;
+        target = diverDown.getTargetInside();
         if (target instanceof LivingEntity){
             LivingEntity livingTarget = (LivingEntity) target;
             livingTarget.addEffect(new EffectInstance(InitEffects.SPRING_LEGS.get(), 1200, 1, false, false, true));
         }
-        DiverDownEntity diverDown = (DiverDownEntity) stand;
         diverDown.setTargetInside(null);
         return super.punchEntity(stand, target, dmgSource)
-                .addKnockback(0.5F + stand.getLastHeavyFinisherValue())
-                .disableBlocking((float) stand.getProximityRatio(target) - 0.25F);
+                .addKnockback(0.5F + stand.getLastHeavyFinisherValue());
     }
 }
