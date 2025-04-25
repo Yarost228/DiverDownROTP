9d5d36841a9378ff4c98b95c2f51b35c4740f7af
Yarost228 <yarik.alferov1@gmail.com>
2025-04-25 21:57:51 +0300

    Changed offset from user

--- src/main/java/com/hk47bot/rotp_dd/action/stand/DiverDownBlock.java
+++ src/main/java/com/hk47bot/rotp_dd/action/stand/DiverDownBlock.java
@@ -2,12 +2,9 @@ package com.hk47bot.rotp_dd.action.stand;
 
 import com.github.standobyte.jojo.action.stand.StandEntityAction;
 import com.github.standobyte.jojo.action.stand.StandEntityBlock;
-import com.github.standobyte.jojo.power.impl.stand.IStandPower;
 
 public class DiverDownBlock extends StandEntityBlock {
     public DiverDownBlock(StandEntityAction.Builder builder){
-        super(builder.noResolveUnlock().standOffsetFront());
+        super(builder.standOffsetFromUser(0, 0.75));
     }
-    @Override
-    public boolean isUnlocked(IStandPower power){return true;}
 }
